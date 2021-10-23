aa796ecc20a176e4a7d7559b5d03b9b06b77e009
jamin-git <jacobamin77@gmail.com>
2021-10-23 15:23:27 -0400

    Trying Website Revamp

--- animation.js
+++ animation.js
@@ -1,41 +1,52 @@
 // Script Not Being Used Currently
 
-var square = document.querySelector('.square');
-var squareAnimation = anime({
-    targets: square,
-    top: [
-        {value: "15%", duration: 6000},
-        {value: "-15%", duration: 6000}
-    ],
-    left: [
-        {value: "5%", duration: 6000},
-        {value: "0", duration: 6000}
-    ],
-    rotateZ: [
-        {value: "360", duration: 12000},
-    ],
-    easing: 'easeInOutSine',
-    loop: true
-});
+// var square = document.querySelector('.square');
+// var squareAnimation = anime({
+//     targets: square,
+//     top: [
+//         {value: "15%", duration: 6000},
+//         {value: "-15%", duration: 6000}
+//     ],
+//     left: [
+//         {value: "5%", duration: 6000},
+//         {value: "0", duration: 6000}
+//     ],
+//     rotateZ: [
+//         {value: "360", duration: 12000},
+//     ],
+//     easing: 'easeInOutSine',
+//     loop: true
+// });
 
-var circle = document.querySelector(".circle");
-var circleAnimation = anime({
-    targets: circle,
-    top: [
-        {value: "-200px", duration: 4500},
-        {value: "1300px", duration: 7500}
-    ],
-    easing: 'easeInOutSine',
-    loop: true
-});
+// var circle = document.querySelector(".circle");
+// var circleAnimation = anime({
+//     targets: circle,
+//     top: [
+//         {value: "-200px", duration: 4500},
+//         {value: "1300px", duration: 7500}
+//     ],
+//     easing: 'easeInOutSine',
+//     loop: true
+// });
 
-var bSquare = document.querySelector(".bSquare");
-var bSquareAnimation = anime({
-    targets: bSquare,
-    translateX: [
-        {value: 100, duration: 2000},
-    ],
-    translateY: [
-        {value: 100, duration: 2000},
-    ],
+// var bSquare = document.querySelector(".bSquare");
+// var bSquareAnimation = anime({
+//     targets: bSquare,
+//     translateX: [
+//         {value: 100, duration: 2000},
+//     ],
+//     translateY: [
+//         {value: 100, duration: 2000},
+//     ],
+// });
+
+var border1 = document.querySelector("#animated-border");
+console.log(border1);
+var borderAnimation = anime({
+    targets: border1,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuad',
+    duration: 5000,
+    direction: 'alternate',
+    loop: true
 });
